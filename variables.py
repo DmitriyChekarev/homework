55c8c3c3b7dc11174516cfd867def90ee10bb016
Дмитрий <chekarevdmitry@yandex.ru>
2024-11-20 13:30:42 +0400

    20/11

--- variables.py
+++ variables.py
@@ -1,4 +1,6 @@
 Number_of_completed_homework = 12
 Number_of_hours_spent = 1.5
 CourseName = 'Python'
-print(f'Курс: Python, всего задач: {Number_of_completed_homework},', f'затрачено часов: {Number_of_hours_spent},', f'среднее время выполнения: {Number_of_hours_spent/Number_of_completed_homework}.')
+print(f'Курс: Python, всего задач: {Number_of_completed_homework},',
+      f'затрачено часов: {Number_of_hours_spent},',
+      f'среднее время выполнения: {Number_of_hours_spent/Number_of_completed_homework}.')
