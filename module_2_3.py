6d58beb709f6ab522fcf8a7048d7907f3cde9c1d
Дмитрий <chekarevdmitry@yandex.ru>
2024-11-26 11:49:16 +0400

    26/11

--- module_2_3.py
+++ module_2_3.py
@@ -1,12 +1,12 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-a1 = 0
+count = 0
 
-while a1 < len(my_list):
-    a2 = my_list[a1]
-    a1 = a1 + 1
-    if a2 == 0:
+while count < len(my_list):
+    number = my_list[count]
+    count = count + 1
+    if number == 0:
         continue
-    elif a2 < 0:
+    elif number < 0:
         break
     else:
-        print(a2)
+        print(number)
