b00e729fbf65043d7d0ef03622a8bae1ed488961
Дмитрий <chekarevdmitry@yandex.ru>
2024-12-12 11:03:02 +0400

    12/12

--- module_4_1.py
+++ module_4_1.py
@@ -1,10 +1,10 @@
-from fake_math import *
-from true_math import *
+from fake_math import fake_divide as fd
+from true_math import true_divide as tm
 
-result1 = fake_divide(69, 3)
-result2 = fake_divide(3, 0)
-result3 = true_divide(49, 7)
-result4 = true_divide(15, 0)
+result1 = fd(69, 3)
+result2 = fd(3, 0)
+result3 = tm(49, 7)
+result4 = tm(15, 0)
 
 print(result1)
 print(result2)
