5e5f187a12b33118b33c151e7682fd814793b9f8
Дмитрий <chekarevdmitry@yandex.ru>
2025-01-13 20:36:02 +0400

    13/01/25

---
+++ module_1_Базовые_структуры_данных/module_1_2_Переменные/module_1_2.py
@@ -0,0 +1,6 @@
+number_of_completed_homework = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+print(f'Курс: Python, всего задач: {number_of_completed_homework},',
+      f'затрачено часов: {number_of_hours_spent},',
+      f'среднее время выполнения: {number_of_hours_spent/number_of_completed_homework}.')
