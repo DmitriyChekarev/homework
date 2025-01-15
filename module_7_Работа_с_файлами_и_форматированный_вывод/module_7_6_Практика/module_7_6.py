300dfd0863d58f1807470992e00d2c10d94017d7
Dmitry <chekarevdmitry@gmail.com>
2025-01-15 23:22:19 +0400

    15/01/25

--- module_7_Работа_с_файлами_и_форматированный_вывод/module_7_6_Практика/module_7_6.py
+++ module_7_Работа_с_файлами_и_форматированный_вывод/module_7_6_Практика/module_7_6.py
@@ -1,4 +1,13 @@
 import tkinter
+from tkinter import filedialog
+import os
+
+
+def file_select():
+    filename = filedialog.askopenfilename(initialdir='/', title='Выберите файл', filetypes=(('Текстовый файл', '.txt'),
+                                                                                            ('Все файлы', '*')))
+    text['text'] = text['text'] + '' + filename
+    os.startfile(filename)
 
 window = tkinter.Tk()
 window.title('Проводник')
@@ -8,6 +17,7 @@ window.resizable(False,False)
 window.attributes('-alpha', 0.9)
 text = tkinter.Label(window, text='Файл:', height=5, width=20, background='#B3E5FC')
 text.grid(column=1, row=1)
-button_select=tkinter.Button(window, width=20, height=3, text='Выбрать файл', background='#B3E5FC')
+button_select=tkinter.Button(window, width=20, height=3, text='Выбрать файл', background='#B3E5FC',
+                             command=file_select())
 button_select.grid(column=1, row=2)
 window.mainloop()
